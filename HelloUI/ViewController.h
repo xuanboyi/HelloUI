2b55ec17af14d7a3cc3128b2ab67a9bf8eb6c600
玄博屹 <genhiroshi@xuanboyidemacbook-pro.local>
2017-12-20 09:43:13 +0800

    Initial Commit

---
+++ HelloUI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloUI
+//
+//  Created by 玄博屹 on 2017/12/20.
+//  Copyright © 2017年 Xuan,boyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HelloUI/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HelloUI
+//
+//  Created by 玄博屹 on 2017/12/20.
+//  Copyright © 2017年 Xuan,boyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
